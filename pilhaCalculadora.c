d306c35a15302a1405cf386a78d5de036a9f41f7
Gustavo Marques <gugamello2014@yahoo.com>
2021-07-25 12:32:04 -0300

    Adicionado comentários às funções

--- pilhaCalculadora.c
+++ pilhaCalculadora.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include "pilhaCaluladora.h"
 
-
+//Função responsável por criar uma pilha vazia
 void criaPilhaVazia(Pilha *pilha) {
     pilha->topo = (Celula *) malloc(sizeof(Celula));
     pilha->fundo = pilha->topo;
@@ -11,10 +11,12 @@ void criaPilhaVazia(Pilha *pilha) {
     pilha->tamanho = 0;
 }
 
+//Verifica se a pilha recebida está vazia
 int estaVazia(Pilha pilha) {
     return (pilha.tamanho == 0);
 }
 
+//Empilha um item no topo da pilha
 int empilha(Pilha *pPilha, Item *pItem) {
     Celula *pNovo;
     pNovo = (Celula *) malloc(sizeof(Celula));
@@ -25,6 +27,7 @@ int empilha(Pilha *pPilha, Item *pItem) {
     return 1;
 }
 
+//Desempilha item no topo da pilha
 int desempilha(Pilha *pPilha, Item *pItem) {
     Celula *pAux;
 
@@ -39,11 +42,14 @@ int desempilha(Pilha *pPilha, Item *pItem) {
     return 1;
 }
 
+//Função responsável por validar a expressão infixa digitada pelo usuário
 int verificaExpressao(char expr[]) {
     int contAbre = 0, contFecha = 0;
 
+    //Verifica se a expressão se inicia com um abre parênteses
     if (expr[0] != '(') return 0;
 
+    //Verifica se a quantidade de parênteses abertos são o mesmo dos fechados
     if (expr[0] == '(') {
         for (int i = 0; i < strlen(expr); ++i) {
             if (expr[i] == '(') contAbre++;
@@ -54,25 +60,36 @@ int verificaExpressao(char expr[]) {
     return (contAbre == contFecha);
 }
 
+//Função responsável por verificar se o caractere recebido é uma operação válida
 int verificaSimboloMatematico(char *valor) {
     if (valor[0] == '+' || valor[0] == '-' || valor[0] == '*' || valor[0] == '/') return 1;
     return 0;
 }
 
+//Função responsável por realizar as contas da expressao pós-fixa
 void resolveExpressao(Pilha *pilha, char *x) {
     Item i1, i2, iAux;
 
+    //Percorre a expressão pós-fixa
     for (int i = 0; x[i] != '\0'; ++i) {
+
+        //Verifica se a string está em um espaço e se a próxima posição não é uma operação
         if (x[i] == ' ' && !verificaSimboloMatematico(&x[i + 1])) {
             iAux.valor = 0;
             empilha(pilha, &iAux);
             imprime(*pilha);
-        } else if (x[i] != ' ' && !verificaSimboloMatematico(&x[i])) {
+        }
+
+            //Verifica se a string está na posição de um número
+        else if (x[i] != ' ' && !verificaSimboloMatematico(&x[i])) {
             desempilha(pilha, &iAux);
             iAux.valor = iAux.valor * 10 + (x[i] - '0');
             empilha(pilha, &iAux);
             imprime(*pilha);
-        } else if (x[i] != ' ') {
+        }
+
+            //Verifica se está em uma operação e então realiza as contas
+        else if (x[i] != ' ') {
             desempilha(pilha, &i1);
             desempilha(pilha, &i2);
 
@@ -110,27 +127,29 @@ void resolveExpressao(Pilha *pilha, char *x) {
     }
 }
 
+//Transforma a expressão infixa digitada pelo usuário para pós-fixa
 char *infixaParaPosfixa(char *exp) {
-    int n = strlen(exp);
-    char *posFix;
+    int n = strlen(exp), j = 0;
+    char *posFixa;
     Pilha pAux;
     Item iAux;
 
     criaPilhaVazia(&pAux);
 
-    posFix = (char *) malloc((n + 1) * sizeof(char));
+    posFixa = (char *) malloc((n + 1) * sizeof(char));
     iAux.valor = (int) exp[0];
+
     empilha(&pAux, &iAux);
 
+    //Caso a expressão não seja válida, o programa é interrompido
     if (!verificaExpressao(exp)) {
         printf("Expressao invalida");
         exit(1);
     }
 
-    int j = 0;
+    //Percorre a expressão infixa e então a transforma para pós-fixa
     for (int i = 0; exp[i] != '\n'; i++) {
-        if (i == 0)
-            posFix[j++] = ' ';
+        if (i == 0) posFixa[j++] = ' ';
         switch (exp[i]) {
             case '(':
                 iAux.valor = (int) exp[i];
@@ -139,17 +158,17 @@ char *infixaParaPosfixa(char *exp) {
             case ')':
                 desempilha(&pAux, &iAux);
                 while (iAux.valor != '(') {
-                    posFix[j++] = ' ';
-                    posFix[j++] = (char) iAux.valor;
+                    posFixa[j++] = ' ';
+                    posFixa[j++] = (char) iAux.valor;
                     desempilha(&pAux, &iAux);
                 }
                 break;
             case '+':
             case '-':
-                posFix[j++] = ' ';
+                posFixa[j++] = ' ';
                 desempilha(&pAux, &iAux);
                 while (iAux.valor != '(') {
-                    posFix[j++] = (char) iAux.valor;
+                    posFixa[j++] = (char) iAux.valor;
                     desempilha(&pAux, &iAux);
                 }
                 empilha(&pAux, &iAux);
@@ -158,10 +177,10 @@ char *infixaParaPosfixa(char *exp) {
                 break;
             case '*':
             case '/':
-                posFix[j++] = ' ';
+                posFixa[j++] = ' ';
                 desempilha(&pAux, &iAux);
                 while (iAux.valor != '(' && iAux.valor != '+' && iAux.valor != '-') {
-                    posFix[j++] = (char) iAux.valor;
+                    posFixa[j++] = (char) iAux.valor;
                     desempilha(&pAux, &iAux);
                 }
                 empilha(&pAux, &iAux);
@@ -169,15 +188,15 @@ char *infixaParaPosfixa(char *exp) {
                 empilha(&pAux, &iAux);
                 break;
             default:
-                posFix[j++] = exp[i];
+                posFixa[j++] = exp[i];
         }
     }
-    posFix[j] = '\0';
+    posFixa[j] = '\0';
     imprime(pAux);
-    return posFix;
+    return posFixa;
 }
 
-
+//Imprime a pilha na tela
 void imprime(Pilha pilha) {
     pilha.topo = pilha.topo->proximo;
     printf("Pilha: \n");
